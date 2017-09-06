f38f9cf26c5110d6b6d5e0a961c6c8b3d0c9d3e7
Raymond Haryanto <raymondharyanto@yahoo.com>
2017-09-06 15:28:40 +0700

    connect to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -27,8 +27,25 @@ export const store = new Vuex.Store({
 		}
 
 	},
-	mutations: {},
-	actions: {},
+	mutations: {
+		createMeetup (state, payload){
+			state.loadedMeetups.push(payload)
+		}
+	},
+	actions: {
+		createMeetup ({commit}, payload){
+			const meetup = {
+				title      : payload.title,
+				location   : payload.location,
+				imageUrl   : payload.imageUrl,
+				description: payload.description,
+				date       : payload.date,
+				id         : 'harcodeId'
+			}
+			// Reach firebase 
+			commit('createMeetup', meetup)
+		}
+	},
 	getters: {
 		loadedMeetups (state){
 			return state.loadedMeetups.sort( (meetupA, meetupB) => {
